ccfd4b1f34bcee57d81b3688e5c89317a86d0533
Ashika Chakravorty <ashika.chak@outlook.com>
2024-05-16 01:05:12 +0200

    Smallest Common Multiple

---
+++ Intermediate_Algorithm_Scripting/ias_14.js
@@ -0,0 +1,28 @@
+// Smallest Common Multiple
+
+function smallestCommons(arr) {
+    const [min, max] = arr.sort((a, b) => a - b);
+    const numberDivisors = max - min + 1;
+  
+    let upperBound = 1;
+    for(let i = min; i<= max; i++) {
+        upperBound *= i;
+    }
+
+    // test all multiples of max 
+    for( let multiple = max; multiple <= upperBound; multiple +=max) {
+        // Check if every value in range divides 'multiple'
+        let divisorCount = 0;
+        for (let i = min; i<= max; i++) {
+             // Count divisors
+            if(multiple % i ===0) {
+                divisorCount++;
+            }
+        }
+        if (divisorCount === numberDivisors) {
+            return multiple;
+        }
+    }
+  }
+  
+  console.log(smallestCommons([1,5]));
