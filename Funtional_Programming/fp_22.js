abb077df63700b164c16c7b5a73d01edc63e0f91
Ashika Chakravorty <ashika.chak@outlook.com>
2024-05-15 16:44:42 +0200

    Functional Programming part 22

---
+++ Funtional_Programming/fp_22.js
@@ -0,0 +1,13 @@
+// Use the some Method to Check that Any Elements in an Array Meet a Criteria
+
+function checkPositive(arr) {
+    // Only change code below this line
+    
+    return arr.some(function(curr) {
+        return curr > 0;
+    })
+  
+    // Only change code above this line
+  }
+  
+  checkPositive([1, 2, 3, -4, 5]);
