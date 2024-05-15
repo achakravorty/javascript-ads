873f20838eb76b02fe120d5ee49aaafebd59beb1
Ashika Chakravorty <ashika.chak@outlook.com>
2024-05-16 00:35:20 +0200

    Sum All Odd Fibonacci Numbers

---
+++ Intermediate_Algorithm_Scripting/ias_12.js
@@ -0,0 +1,21 @@
+// Sum All Odd Fibonacci Numbers
+
+
+function sumFibs(num) {
+
+    let fibArr = [];
+    let n1 =0, n2=1, nextTerm=0;
+    nextTerm = n1 + n2;
+    fibArr.push(0);
+    fibArr.push(1);
+    while(nextTerm <= num) {
+        fibArr.push(nextTerm);
+        n1 = n2;
+        n2 = nextTerm;
+        nextTerm = n1 + n2;
+    }
+    const oddFibArr = fibArr.filter(number => number <= num && !(number%2 ==0));
+    return oddFibArr.reduce((sum, currentval) => sum + currentval, 0);
+}
+  
+console.log(sumFibs(75025));
