ddc5b3ac628c8d8cd988c27825c61a6f3b2548ba
Ashika Chakravorty <ashika.chak@outlook.com>
2024-05-17 17:21:51 +0200

    Arguments Optional

---
+++ Intermediate_Algorithm_Scripting/ias_19.js
@@ -0,0 +1,35 @@
+// Arguments Optional
+
+function addTogether() {
+    const [first, second] = arguments;
+
+    if(typeof(first) === "number") {
+        if(typeof(second) === "number") {
+            return first + second;
+        }
+        if(arguments.length === 1) return (second) => addTogether(first, second);
+    }
+
+
+
+    // if(args.length ==0) {
+    //     return undefined;
+    // }
+    // if(args.length ==1 && typeof(args[0]) == "number") {
+    //     const sumTwoAnd = function(y) {
+    //         return args[1] + y;
+    //     }
+    //     return sumTwoAnd;
+    // }
+
+    // if(typeof(args[0]) != "number" || typeof(args[1]) != "number") {
+    //     return undefined;
+    // }
+
+    // return args[0] + args[1];
+  }
+  
+  console.log(addTogether(2,3));
+  console.log(addTogether(5));
+  console.log(addTogether("https://www.youtube.com/watch?v=dQw4w9WgXcQ"));
+  console.log(addTogether(23.4, 30));
