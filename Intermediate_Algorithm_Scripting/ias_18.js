201796b549ad544444d3569fec78e0b76933b8f6
Ashika Chakravorty <ashika.chak@outlook.com>
2024-05-17 16:59:37 +0200

    Everything Be True

---
+++ Intermediate_Algorithm_Scripting/ias_18.js
@@ -0,0 +1,16 @@
+// Everything Be True
+
+function truthCheck(collection, pre) {
+
+
+    for(let i =0; i< collection.length; i++) {
+        if(!collection[i][pre]) {
+            return false;
+        }
+    }
+
+    return true;
+  }
+  
+  console.log(truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "isBot"));
+  console.log(truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "name"));
