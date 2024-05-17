35937489485a49ede34a489510623f825511e6cd
Ashika Chakravorty <ashika.chak@outlook.com>
2024-05-17 16:52:54 +0200

    Binary Agents

---
+++ Intermediate_Algorithm_Scripting/ias_17.js
@@ -0,0 +1,29 @@
+// Binary Agents
+
+function binaryAgent(str) {
+
+    //split the str into indiviudal binray codes
+    let binaryList = str.split(' ')
+
+    let decimalList = binaryList.map(bin => binaryToDecimal(bin));
+
+    let letterList = decimalList.map(dec => String.fromCharCode(dec));
+
+    return letterList.join('');
+  }
+
+  function binaryToDecimal(str) {
+    let binarySplit = str.split('').reverse();
+
+    let decimal = 0;
+    for(let i =0; i < binarySplit.length; i++) {
+        decimal += parseInt(binarySplit[i]) * Math.pow(2, i);
+    }
+
+    return decimal;
+  }
+  
+//   console.log(binaryToDecimal('01000001'));
+  console.log(binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"));
+
+
